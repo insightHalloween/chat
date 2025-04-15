699e1fbacf095988681911d3a1f59b64e087c8fd
kangzheng <kz2859796815@163.com>
2025-04-15 21:19:40 +0800

    把文件修改了一下

--- ChatServer/main.cpp
+++ ChatServer/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -10,16 +9,3 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
->>>>>>> 16fede4cd6a4bd7b12dba7ec4b7d3a784d44619e
--- ChatServer/mainwindow.h
+++ ChatServer/mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -34,40 +33,4 @@ private:
     ChatServer *server;
 };
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
 
-#include <QMainWindow>
-#include "chatserver.h"
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private slots:
-    void handleClientConnected(const QString &clientAddress);
-    void handleClientDisconnected(const QString &clientAddress);
-    void handleMessageReceived(const QString &from, const QString &message);
-    void handleStopServer();
-    void openClientManager();
-
-private:
-    void appendLog(const QString &log);
-    void updateClientCount();
-
-    Ui::MainWindow *ui;
-    ChatServer *server;
-};
-#endif // MAINWINDOW_H
->>>>>>> 16fede4cd6a4bd7b12dba7ec4b7d3a784d44619e
